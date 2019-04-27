5a53a0dab679482327120388d6cca7787d05b04a
Thomas Raithel <T.Raithel@syzygy.de>
2019-04-27 23:32:07 +0200

    Modify build

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Change to current dir
-cd "$(dirname "$0")"
+# Copy files into current work dir
+cp concourse-test/package.json package.json
+cp concourse-test/package-lock.json package-lock.json
+cp -r concourse-test/src src
 
 # install deps
-npm install
+npm ci
 
 # execute build
 npm run build
