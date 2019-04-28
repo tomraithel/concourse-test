6bc499a3d0bda4078d01d7be4f671c45a6164e62
Thomas Raithel <T.Raithel@syzygy.de>
2019-04-28 22:06:16 +0200

    Update buildscript

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Copy files into current work dir
-cp concourse-test/package.json package.json
-cp concourse-test/package-lock.json package-lock.json
-cp -r concourse-test/src src
+# TODO - find a way how "webrew-app" can be omitted
+cp webrew-app/package.json package.json
+cp webrew-app/package-lock.json package-lock.json
+cp -r webrew-app/src src
 
 # install deps
 npm ci
